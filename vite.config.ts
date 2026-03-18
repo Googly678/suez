487bad7d70f87939372a6d7774576ab4fabd4b65
Goodly678 <867089906@qq.com>
2026-03-18 16:13:49 +0800

    fix(deploy): Add GitHub Pages Action and fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './', // 解决部署到 GitHub Pages 等环境时的路径问题
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
